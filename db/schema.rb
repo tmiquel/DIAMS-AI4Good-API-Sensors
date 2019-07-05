1ef378e9218069893411100f8fc6116259507e9b
Aminesanhaji <thpamine@gmail.com>
2019-07-05 11:07:59 +0200

    migration here

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,26 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_704_141_535) do
+ActiveRecord::Schema.define(version: 2019_07_04_141535) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'data', force: :cascade do |t|
-    t.string 'devise'
-    t.string 'PM2_5'
-    t.string 'PM10'
-    t.integer 'feeling'
-    t.string 'latitude'
-    t.string 'longitude'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "data", force: :cascade do |t|
+    t.string "devise"
+    t.string "PM2_5"
+    t.string "PM10"
+    t.integer "feeling"
+    t.string "latitude"
+    t.string "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end

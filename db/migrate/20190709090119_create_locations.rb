e0bc20786e6cf2ae684acbc29bc7fdf17d08fdda
stug43 <nganta.a@hotmail.fr>
2019-07-09 15:30:05 +0200

    rename location locations and generate scaffold location

--- db/migrate/20190709090119_create_locations.rb
+++ db/migrate/20190709090119_create_locations.rb
@@ -1,8 +1,10 @@
-class CreateLocation < ActiveRecord::Migration[5.2]
+class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
 			t.string :latitude
 			t.string :longitude
+
+			t.timestamps
     end
   end
 end

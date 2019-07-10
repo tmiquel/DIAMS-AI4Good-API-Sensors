01e6bfdff1c5aec2fc08176fdbf63d2345bc937b
stug43 <auguste@alabintheair.cc>
2019-07-10 13:09:54 +0200

    fix: add location parameter to link method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,16 +1,16 @@
 class Location < ApplicationRecord
-	after_create -> { link_pos(self.get_last_datum) }
+	after_create -> { link_pos(self, self.get_last_datum) }
 
 	def matches?(datum, seuil)
-		if ((self.created_at  - datum.created_at) < seuil)
+		if ((self.created_at - datum.created_at) < seuil)
 			return true
 		else
 			return false
 		end
 	end
 
-	def link_pos(datum)
-		if self.matches?(datum, 30)
+	def link_pos(location, datum)
+		if location.matches?(datum, 30)
 			datum.latitude = self.latitude
 			datum.longitude = self.longitude
 			datum.save

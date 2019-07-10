fb513657dabab5f20d43f44c8d3f24ed37a09dcc
stug43 <auguste@alabintheair.cc>
2019-07-10 12:01:42 +0200

    create method to link location to datum once created

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,24 @@
 class Location < ApplicationRecord
+	after_create -> { link_pos(get_last_datum(self)) }
+
+	def matches?(datum, seuil)
+		if ((self.created_at  - datum.created_at) < seuil)
+			return true
+		else
+			return false
+		end
+	end
+
+	def link_pos(datum)
+		if self.matches?(datum, 30)
+			datum.latitude = self.latitude
+			datum.longitude = self.longitude
+			datum.save
+		end
+	end
+
+	def get_last_datum(location)
+		Datum.where(devise: self.device).last
+	end
+
 end

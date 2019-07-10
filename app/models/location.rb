64e6463e40a55095bf1fb65b86e4b776ab2b0815
stug43 <auguste@alabintheair.cc>
2019-07-10 15:28:30 +0200

    fix: execute link function only if location's device match with a datum's device

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,24 +1,20 @@
 class Location < ApplicationRecord
-	after_create -> { link_pos(self, self.get_last_datum) }
+	after_create -> { link_pos(self.get_last_datum) }
 
-	def matches?(datum, seuil)
-		puts "ù"*660
-		puts datum.created_at
-		if ((self.created_at - datum.created_at) < seuil)
-			return true
-		else
-			return false
-		end
-	end
-
-	def link_pos(location, datum)
-		if location.matches?(datum, 30)
-			datum.update(latitude: self.latitude, longitude: self.longitude)
+	def link_pos(datum)
+		if datum
+			if self.matches?(datum, 30)
+				datum.update(latitude: self.latitude, longitude: self.longitude)
+			end
 		end
 	end
 
 	def get_last_datum
-		return Datum.where(devise: self.device).last
+		Datum.where(devise: self.device).last
+	end
+
+	def matches?(datum, seuil)
+		return ((self.created_at - datum.created_at) < seuil)
 	end
 
 end

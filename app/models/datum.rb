a06438d5187f8fdd3518b734451ad488da4da6a7
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-07-10 12:53:54 +0200

    feat - add basic seed

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,3 +1,9 @@
 class Datum < ApplicationRecord
     validates_presence_of :devise
+
+
+    def self.get_last(devise_name)
+    	return Datum.where(devise: devise_name).last
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+	Datum.create(devise: 'anne', 
+		PM2_5: rand(10).to_s,
+		PM10: rand(10).to_s, 
+		positive_feeling: 1, 
+		mixed_feeling: 0,
+		negative_feeling: 0,
+		longitude: "46.1",
+		latitude: "3.2")
+end
+
+10.times do
+	Datum.create(devise: 'remy', 
+		PM2_5: rand(10).to_s,
+		PM10: rand(10).to_s, 
+		positive_feeling: 1, 
+		mixed_feeling: 0,
+		negative_feeling: 0,
+		longitude: "46.1",
+		latitude: "3.2")
+end
+
+

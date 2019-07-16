b410c46bd68096a17070445bec92c1ecf2fcad9d
Aminesanhaji <thpamine@gmail.com>
2019-07-16 09:27:46 +0200

    -feat: add auth to the app (not tested yet)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :locations
   resources :maps
   resources :data
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   
   root 'maps#index'
 

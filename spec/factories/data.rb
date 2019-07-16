587ce84a66c052c1a6b8f204b9ac4b911170570f
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-07-16 17:48:36 +0200

    feat - update HMAC in json_web_token.rb + update factories

--- spec/factories/data.rb
+++ spec/factories/data.rb
@@ -3,7 +3,9 @@ FactoryBot.define do
     devise { Faker::Name.name }
     PM2_5 { rand(10).to_s}
     PM10 {rand(10).to_s }
-    feeling { rand(10) }
+    positive_feeling { rand(10) }
+    mixed_feeling { rand(10) }
+    negative_feeling { rand(10) }
     latitude {  rand(-90.000000000...90.000000000).to_s }
     longitude { rand(-180.000000000...180.000000000).to_s }
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
     factory :user do
       name { Faker::Name.name }
-      email 'foo@bar.com'
-      password 'foobar'
+      email { 'foo@bar.com' }
+      password { 'foobar' }
+      password_confirmation { 'foobar' }
     end
   end

d4b05f6c6863990d82010336f1ca40be9649468b
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-07-27 15:14:35 +0200

    fix issue on fill_null_locations in datum.rb in case there are no previous data with a location for a device"

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -12,13 +12,14 @@ class Datum < ApplicationRecord
     		Datum.where(
     			"device = ? AND latitude IS NOT ? AND id < ?",
     			 self.device, nil, self.id).last
-    	return last_datum_with_loc.latitude, last_datum_with_loc.longitude
+    	return last_datum_with_loc == nil ? [nil, nil] : 
+    	last_datum_with_loc.latitude, last_datum_with_loc.longitude
     end
 
 
 
-    def self.fill_null_locations(device_name)
-		Datum.where("device = ? AND ((latitude IS ?) OR (longitude IS ?))",device_name, nil, nil).
+    def self.fill_null_locations
+		Datum.where("(latitude IS ?) OR (longitude IS ?)", nil, nil).
 		find_each do |datum|
   			datum.update(latitude: datum.get_last_location[0], 
   				longitude: datum.get_last_location[1])
